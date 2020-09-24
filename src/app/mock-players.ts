482cc82945b3e917c7f8085396eda35d226313f2
PXMAN <pxman18011997@gmail.com>
2020-09-24 17:17:31 +0700

    update code

--- src/app/mock-players.ts
+++ src/app/mock-players.ts
@@ -2,13 +2,13 @@ import {Player} from './player';
 
 export const PLAYERS:Player[]=[
     {id:1, name:'Cristiano Ronaldo'},
-    {id:1, name:'Olivier Giroud'},
-    {id:1, name:'Leo Messi'},
-    {id:1, name:'Kylian Mbape'},
-    {id:1, name:'Sadio Mane'},
-    {id:1, name:'Gareth Bale'},
-    {id:1, name:'Kevin De Bruyne'},
-    {id:1, name:'Leroy Sane'},
-    {id:1, name:'Serge Gnabry'},
-    {id:1, name:'Neymar Jr'}
+    {id:2, name:'Olivier Giroud'},
+    {id:3, name:'Leo Messi'},
+    {id:4, name:'Kylian Mbape'},
+    {id:5, name:'Sadio Mane'},
+    {id:6, name:'Gareth Bale'},
+    {id:7, name:'Kevin De Bruyne'},
+    {id:8, name:'Leroy Sane'},
+    {id:9, name:'Serge Gnabry'},
+    {id:10, name:'Neymar Jr'}
 ];
